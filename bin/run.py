00b31f37d6b20943b945c01270e7be9ad2ea9bba
Derek Fang <fangpengdong817@gmail.com>
2014-11-21 16:07:23 +0800

    NEW: 1. Create the lightning project.

---
+++ bin/run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os, sys
+
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(
+        sys.argv[0]), os.pardir, os.pardir))
+if os.path.exists(os.path.join(possible_topdir, "allchat", "__init__.py")):
+    sys.path.insert(0, possible_topdir)
+sys.path.insert(0, possible_topdir)
+
+if __name__ == '__main__':
+    pass
